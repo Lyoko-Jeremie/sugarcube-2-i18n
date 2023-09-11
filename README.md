94f2a7fa83ee10ad31e6ca542b6185a74b09013d
LyokoJeremie <lucheng989898@gmail.com>
2023-09-12 05:49:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+
+
+
+
+generate TS config
+```sh
+yarn run tsc --init
+```
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/init.ts
@@ -0,0 +1,11 @@
+console.log('init');
+
+
+class I18NManager {
+    constructor() {
+        console.log('I18NManager constructor');
+    }
+
+}
+
+const i18nManager = new I18NManager();
